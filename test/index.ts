56e806e396b397d02063f5aae5176ced90a4715f
nichoth <nichoth@nichoth.com>
2025-05-13 13:43:31 -0700

    fix build

--- test/index.ts
+++ test/index.ts
@@ -1,28 +1,29 @@
 import Bowser from 'bowser'
 import { importMap, dynamicImport, staticImport } from '../src/index.js'
 
-const mainEl = document.querySelector('main')
-const importMapOk = importMap()
-const browser = Bowser.parse(window.navigator.userAgent)
-console.log('bowser', JSON.stringify(browser, null, 2))
+(async function () {
+    const mainEl = document.querySelector('main')
+    const importMapOk = importMap()
+    const browser = Bowser.parse(window.navigator.userAgent)
+    console.log('bowser', JSON.stringify(browser, null, 2))
 
-const { name } = browser.browser
-console.log('**browser**', name)
+    const { name } = browser.browser
+    console.log('**browser**', name)
 
-const dynamic = await dynamicImport()
+    const dynamic = await dynamicImport()
+    const staticOk = staticImport()
 
-const staticOk = staticImport()
+    mainEl!.innerHTML = `
+        <h1>browser tests</h1>
+        <h2>${name}</h2>
 
-mainEl!.innerHTML = `
-    <h1>browser tests</h1>
-    <h2>${name}</h2>
+        <h3>import map</h3>
+        <p>${importMapOk}</p>
 
-    <h3>import map</h3>
-    <p>${importMapOk}</p>
+        <h3>dynamic imports</h3>
+        <p>${dynamic}</p>
 
-    <h3>dynamic imports</h3>
-    <p>${dynamic}</p>
-
-    <h3>static imports</h3>
-    <p>${staticOk}</p>
-`
+        <h3>static imports</h3>
+        <p>${staticOk}</p>
+    `
+})()
